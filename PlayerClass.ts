b9d49e5fa4c0afef59b62896561f33476fdb9bfd
gonabe <120723980+gonabe@users.noreply.github.com>
2024-03-18 23:12:45 +0300

    Update PlayerClass.ts
    Viewers are now private and getters- setters added,
    a very basic draft of training function added

--- PlayerClass.ts
+++ PlayerClass.ts
@@ -49,13 +49,13 @@ interface mental {
 }
 
 class Player{
-   goalkeeping: goalkeeping
-   personal: personal
-   history: history
-   current_condition: current_condition
-   technical: technical
-   physical: physical
-   mental: mental
+  private goalkeeping: goalkeeping
+  private personal: personal
+  private history: history
+  private current_condition: current_condition
+  private technical: technical
+  private physical: physical
+  private mental: mental
   constructor(
     Focus: number,
     Reflex: number,
@@ -142,5 +142,54 @@ class Player{
   };
   }
 
+  //goalkeeping getter - setter
+  get goalkeeping_getter(){
+    return(this.goalkeeping)
+  }
+  set goalkeeping_setter(new_goalkeeping : goalkeeping) {
+    this.goalkeeping = new_goalkeeping;
+  }
 
+  //personal setter- getter
+  set personal_setter(new_personal: personal) {
+    this.personal = new_personal;
+  }  
+  get personal_getter(): personal {
+    return this.personal;
+  }
+
+  //history setter- getter
+  set history_setter(new_history: history) {
+    this.history = new_history;
+  } 
+  get history_getter(): history {
+    return this.history;
+  }
+
+  //current_condition setter- getter
+   set current_condition_setter(new_current_condition: current_condition) {
+        this.current_condition = new_current_condition;
+  }
+  get current_condition_getter(): current_condition {
+        return this.current_condition;
+  }
+
+  //technical setter- getter
+  set technical_setter(new_technical: technical) {
+        this.technical = new_technical;
+  }
+  get technical_getter(): technical {
+    return this.technical;
+  } 
+}
+function invokeTraining(player: Player): void {
+  const currentGoalkeeping = player.goalkeeping_getter;
+  const newGoalkeeping = {
+      Focus: currentGoalkeeping.Focus * 2,
+      Reflex: currentGoalkeeping.Reflex * 2,
+      Handling: currentGoalkeeping.Handling * 2,
+      Kicking: currentGoalkeeping.Kicking * 2,
+      one_on_one: currentGoalkeeping.one_on_one * 2
+  };
+  player.goalkeeping_setter = newGoalkeeping;
 }
